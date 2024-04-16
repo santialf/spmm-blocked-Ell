148fe5bd015d78d3eecc2cc676bd30486973afd9
santialf <91532972+santialf@users.noreply.github.com>
2024-04-16 11:51:33 +0100

    main file and makefile

---
+++ spmm_blockedell_example.cpp
@@ -0,0 +1,474 @@
+/*
+ * Copyright 1993-2022 NVIDIA Corporation.  All rights reserved.
+ *
+ * NOTICE TO LICENSEE:
+ *
+ * This source code and/or documentation ("Licensed Deliverables") are
+ * subject to NVIDIA intellectual property rights under U.S. and
+ * international Copyright laws.
+ *
+ * These Licensed Deliverables contained herein is PROPRIETARY and
+ * CONFIDENTIAL to NVIDIA and is being provided under the terms and
+ * conditions of a form of NVIDIA software license agreement by and
+ * between NVIDIA and Licensee ("License Agreement") or electronically
+ * accepted by Licensee.  Notwithstanding any terms or conditions to
+ * the contrary in the License Agreement, reproduction or disclosure
+ * of the Licensed Deliverables to any third party without the express
+ * written consent of NVIDIA is prohibited.
+ *
+ * NOTWITHSTANDING ANY TERMS OR CONDITIONS TO THE CONTRARY IN THE
+ * LICENSE AGREEMENT, NVIDIA MAKES NO REPRESENTATION ABOUT THE
+ * SUITABILITY OF THESE LICENSED DELIVERABLES FOR ANY PURPOSE.  IT IS
+ * PROVIDED "AS IS" WITHOUT EXPRESS OR IMPLIED WARRANTY OF ANY KIND.
+ * NVIDIA DISCLAIMS ALL WARRANTIES WITH REGARD TO THESE LICENSED
+ * DELIVERABLES, INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY,
+ * NONINFRINGEMENT, AND FITNESS FOR A PARTICULAR PURPOSE.
+ * NOTWITHSTANDING ANY TERMS OR CONDITIONS TO THE CONTRARY IN THE
+ * LICENSE AGREEMENT, IN NO EVENT SHALL NVIDIA BE LIABLE FOR ANY
+ * SPECIAL, INDIRECT, INCIDENTAL, OR CONSEQUENTIAL DAMAGES, OR ANY
+ * DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS,
+ * WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS
+ * ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE
+ * OF THESE LICENSED DELIVERABLES.
+ *
+ * U.S. Government End Users.  These Licensed Deliverables are a
+ * "commercial item" as that term is defined at 48 C.F.R. 2.101 (OCT
+ * 1995), consisting of "commercial computer software" and "commercial
+ * computer software documentation" as such terms are used in 48
+ * C.F.R. 12.212 (SEPT 1995) and is provided to the U.S. Government
+ * only as a commercial end item.  Consistent with 48 C.F.R.12.212 and
+ * 48 C.F.R. 227.7202-1 through 227.7202-4 (JUNE 1995), all
+ * U.S. Government End Users acquire the Licensed Deliverables with
+ * only those rights set forth herein.
+ *
+ * Any use of the Licensed Deliverables in individual and commercial
+ * software must include, in the user documentation and internal
+ * comments to the code, the above Disclaimer and U.S. Government End
+ * Users Notice.
+ */
+#include <cuda_fp16.h>        // data types
+#include <cuda_runtime_api.h> // cudaMalloc, cudaMemcpy, etc.
+#include <cusparse.h>         // cusparseSpMM
+#include <cstdio>            // printf
+#include <cstdlib>           // EXIT_FAILURE
+
+#include <string.h>
+#include <time.h>
+#include <set>
+#include <iostream>
+#include <fstream>
+
+#include "mmio.c"
+#include "smsh.c"
+
+#define CHECK_CUDA(func)                                                       \
+{                                                                              \
+    cudaError_t status = (func);                                               \
+    if (status != cudaSuccess) {                                               \
+        std::printf("CUDA API failed at line %d with error: %s (%d)\n",        \
+               __LINE__, cudaGetErrorString(status), status);                  \
+        return EXIT_FAILURE;                                                   \
+    }                                                                          \
+}
+
+#define CHECK_CUSPARSE(func)                                                   \
+{                                                                              \
+    cusparseStatus_t status = (func);                                          \
+    if (status != CUSPARSE_STATUS_SUCCESS) {                                   \
+        std::printf("CUSPARSE API failed at line %d with error: %s (%d)\n",    \
+               __LINE__, cusparseGetErrorString(status), status);              \
+        return EXIT_FAILURE;                                                   \
+    }                                                                          \
+}
+
+const int EXIT_UNSUPPORTED = 2;
+
+__half* createRandomArray(int n) {
+    __half* array = new __half[n];
+
+    for (int i = 0; i < n; i++) { 
+        array[i] = 1.0;
+    }
+
+    return array;
+}
+
+/* Finds the possible amount of column blocks the matrix can have */
+int findMaxNnz(int *rowPtr, int *colIndex, int num_rows, int block_size) {
+
+    int max = 0;
+    int num_blocks = num_rows / block_size;
+
+    std::set<int> mySet;
+
+    for(int i=0; i < num_blocks; i++) {
+
+        for (int j = 0; j<block_size; j++) {
+            int id = block_size*i+j;
+            
+            for(int k=rowPtr[id]; k<rowPtr[id+1]; k++)
+                mySet.insert(colIndex[k]/block_size);
+            
+            if (mySet.size() > max)
+                max = mySet.size();
+        }
+        mySet.clear();
+    }
+
+    return max*block_size;
+}
+
+/* Creates the array of block indexes for the blocked ell format */
+int *createBlockIndex(int *rowPtr, int *colIndex, int num_rows, int block_size, int ell_cols) {
+
+    long int mb = num_rows/block_size, nb = ell_cols/block_size;
+    if (num_rows % block_size != 0)
+        mb++;
+
+    int* hA_columns = new int[nb*mb]();
+    int ctr = 0;
+
+    memset(hA_columns, -1, (long int) nb * mb * sizeof(int));
+    std::set<int> mySet;
+
+    /* Goes through the blocks of the matrix of block_size */
+    for(int i=0; i<mb; i++) {
+
+        /* Iterates through the rows of each block */
+        for (int j = 0; j < block_size; j++) {
+            int id = block_size*i + j;
+            int index = 0;
+            if (id >= num_rows)
+                break;
+
+            /* Iterates over the nnzs of each row */
+            for(int k=rowPtr[id]; k<rowPtr[id+1]; k++) {    
+                index = (colIndex[k]/block_size);
+                mySet.insert(index);
+            }
+        }
+        for (int elem : mySet)
+            hA_columns[ctr++] = elem;
+        
+        ctr = i*nb+nb;
+        mySet.clear();
+    }
+    return hA_columns; 
+}
+
+/* Creates the array of values for the blocked ell format */
+__half *createValueIndex(int *rowPtr, int *colIndex, float *values, int *hA_columns, int num_rows, int block_size, int ell_cols) {
+
+    /* Allocate enough memory for the array */
+    __half* hA_values = new __half[(long int)num_rows * ell_cols]();
+
+    long int mb = num_rows/block_size, nb = ell_cols/block_size;
+    if (num_rows % block_size != 0)
+        mb++;
+
+    /* Set all values to 0 */
+    memset(hA_values, 0, (long int) num_rows * ell_cols * sizeof(__half));
+
+    /* Iterate the blocks in the y axis */
+    for (int i=0; i<mb;i++){
+
+        /* Iterate the lines of each block */
+        for (int l = 0; l<block_size; l++) {
+            int ctr = 0;
+
+            /* Iterate the blocks in the block_id array (x axis) */
+            for (int j = 0; j < nb; j++) {
+                int id = nb*i + j;
+                if (hA_columns[id] == -1)
+                    break;
+
+                /* Iterate each line of the matrix */
+                for(int k=rowPtr[i*block_size+l]; k<rowPtr[i*block_size+l+1]; k++) {  
+
+                    /* If the element is not in the same block, skip*/
+                    if (colIndex[k]/block_size > hA_columns[id])
+                        break;
+                    else if (colIndex[k]/block_size == hA_columns[id]) 
+                        hA_values[(long int)i*ell_cols*block_size+l*ell_cols+j*block_size+(colIndex[k]-(hA_columns[id]*block_size))] = values[k];
+                }
+            }
+        }
+    }
+    
+    return hA_values;
+}
+
+int main(int argc, char *argv[]) {
+
+    int ret_code;
+    MM_typecode matcode;
+    FILE *f;
+    int A_num_rows, A_num_cols, nz, A_nnz;
+    int i = 0, *I_complete, *J_complete;
+    float *V_complete;
+    
+    	/* READ MTX FILE INTO CSR MATRIX */
+    /************************************************************************************************************/
+    if ((f = fopen(argv[1], "r")) == NULL)
+    {
+        printf("Could not locate the matrix file. Please make sure the pathname is valid.\n");
+        exit(1);
+    }
+
+    if (mm_read_banner(f, &matcode) != 0)
+    {
+        printf("Could not process Matrix Market banner.\n");
+        exit(1);
+    }
+    matcode[4] = '\0';
+    
+    if ((ret_code = mm_read_mtx_crd_size(f, &A_num_rows, &A_num_cols, &nz)) != 0)
+    {
+        printf("Could not read matrix dimensions.\n");
+        exit(1);
+    }
+    
+    if ((strcmp(matcode, "MCRG") == 0) || (strcmp(matcode, "MCIG") == 0) || (strcmp(matcode, "MCPG") == 0) || (strcmp(matcode, "MCCG") == 0))
+    {
+
+        I_complete = (int *)calloc(nz, sizeof(int));
+        J_complete = (int *)calloc(nz, sizeof(int));
+        V_complete = (float *)calloc(nz, sizeof(float));
+
+        for (i = 0; i < nz; i++)
+        {
+            if (matcode[2] == 'P') {
+                fscanf(f, "%d %d", &I_complete[i], &J_complete[i]);
+                V_complete[i] = 1;
+            }  
+            else {
+                fscanf(f, "%d %d %f", &I_complete[i], &J_complete[i], &V_complete[i]);
+            } 
+            fscanf(f, "%*[^\n]\n");
+            /* adjust from 1-based to 0-based */
+            I_complete[i]--;
+            J_complete[i]--;
+        }
+    }
+
+    /* If the matrix is symmetric, we need to construct the other half */
+
+    else if ((strcmp(matcode, "MCRS") == 0) || (strcmp(matcode, "MCIS") == 0) || (strcmp(matcode, "MCPS") == 0) || (strcmp(matcode, "MCCS") == 0) || (strcmp(matcode, "MCCH") == 0) || (strcmp(matcode, "MCRK") == 0) || (matcode[0] == 'M' && matcode[1] == 'C' && matcode[2] == 'P' && matcode[3] == 'S'))
+    {
+
+        I_complete = (int *)calloc(2 * nz, sizeof(int));
+        J_complete = (int *)calloc(2 * nz, sizeof(int));
+        V_complete = (float *)calloc(2 * nz, sizeof(float));
+
+        int i_index = 0;
+
+        for (i = 0; i < nz; i++)
+        {
+            if (matcode[2] == 'P') {
+                fscanf(f, "%d %d", &I_complete[i], &J_complete[i]);
+                V_complete[i] = 1;
+            }
+            else {
+                fscanf(f, "%d %d %f", &I_complete[i], &J_complete[i], &V_complete[i]);
+            }
+                
+            fscanf(f, "%*[^\n]\n");
+
+            if (I_complete[i] == J_complete[i])
+            {
+                /* adjust from 1-based to 0-based */
+                I_complete[i]--;
+                J_complete[i]--;
+            }
+            else
+            {
+                /* adjust from 1-based to 0-based */
+                I_complete[i]--;
+                J_complete[i]--;
+                J_complete[nz + i_index] = I_complete[i];
+                I_complete[nz + i_index] = J_complete[i];
+                V_complete[nz + i_index] = V_complete[i];
+                i_index++;
+            }
+        }
+        nz += i_index;
+    }
+    else
+    {
+        printf("This matrix type is not supported: %s \n", matcode);
+        exit(1);
+    }
+
+    /* sort COO array by the rows */
+    if (!isSorted(J_complete, I_complete, nz)) {
+        quicksort(J_complete, I_complete, V_complete, nz);
+    }
+    
+    /* Convert from COO to CSR */
+    int* rowPtr = new int[A_num_rows + 1]();
+    int* colIndex = new int[nz]();
+    float* values = new float[nz]();
+
+    for (i = 0; i < nz; i++) {
+        colIndex[i] = J_complete[i];
+        values[i] = V_complete[i];
+        rowPtr[I_complete[i] + 1]++;
+    }
+    for (i = 0; i < A_num_rows; i++) {
+        rowPtr[i + 1] += rowPtr[i];
+    }
+    A_nnz = nz;
+
+    free(I_complete);
+    free(J_complete);
+    free(V_complete);
+    fclose(f);
+    /* MTX READING IS FINISH */
+    /************************************************************************************************************/
+
+    // Host problem definition
+    int   A_ell_blocksize = 16;
+    
+    int * rowPtr_pad;
+    int remainder = A_num_rows % A_ell_blocksize;
+    if (remainder != 0) {
+        A_num_rows = A_num_rows + (A_ell_blocksize - remainder);
+        A_num_cols = A_num_cols + (A_ell_blocksize - remainder);
+        rowPtr_pad = new int[A_num_rows + 1];
+        for (int i=0; i<A_num_rows - (A_ell_blocksize - remainder); i++)
+            rowPtr_pad[i] = rowPtr[i];
+        for (int j=A_num_rows - (A_ell_blocksize - remainder); j<A_num_rows + 1; j++)
+            rowPtr_pad[j] = nz;
+        delete[] rowPtr;
+    } else {
+        rowPtr_pad = rowPtr;
+    }   
+    
+    int   A_ell_cols      = findMaxNnz(rowPtr_pad, colIndex, A_num_rows, A_ell_blocksize);
+    long int   A_num_blocks    = A_ell_cols * A_num_rows /
+                           (A_ell_blocksize * A_ell_blocksize);
+    int   B_num_rows      = A_num_cols;
+    int   B_num_cols      = 32;
+    int   ldb             = B_num_rows;
+    int   ldc             = A_num_rows;
+    int   B_size          = ldb * B_num_cols;
+    int   C_size          = ldc * B_num_cols;
+
+    /* Memory occupied by blocked ell vectors */
+    float mem_ids = (float) A_num_blocks * sizeof(int) / 1000000000;
+    float mem_values = (float) A_ell_cols * A_num_rows * sizeof(float) / 1000000000;
+    /*if (mem_ids+mem_values > 30) {
+      printf("Too much memory :(\n");
+      return 1;
+    }*/
+
+    int   *hA_columns     = createBlockIndex(rowPtr_pad, colIndex, A_num_rows, A_ell_blocksize, A_ell_cols);
+    __half *hA_values     = createValueIndex(rowPtr_pad, colIndex, values, hA_columns, A_num_rows, A_ell_blocksize, A_ell_cols);
+
+    __half *hB            = createRandomArray(B_size);
+    __half *hC            = new __half[(long int) C_size*sizeof(__half)];
+
+    float alpha           = 1.0f;
+    float beta            = 0.0f;
+
+    delete[] rowPtr_pad;
+    delete[] colIndex;
+    delete[] values;
+    //--------------------------------------------------------------------------
+    // Check compute capability
+    cudaDeviceProp props;
+    CHECK_CUDA( cudaGetDeviceProperties(&props, 0) )
+    if (props.major < 7) {
+      std::printf("cusparseSpMM with blocked ELL format is supported only "
+                  "with compute capability at least 7.0\n");
+      return EXIT_UNSUPPORTED;
+    }
+    //--------------------------------------------------------------------------
+    // Device memory management
+    int    *dA_columns;
+    __half *dA_values, *dB, *dC;
+    CHECK_CUDA( cudaMalloc((void**) &dA_columns, (long int) A_num_blocks * sizeof(int)) )
+    CHECK_CUDA( cudaMalloc((void**) &dA_values,
+                                    (long int) A_ell_cols * A_num_rows * sizeof(__half)) )
+    CHECK_CUDA( cudaMalloc((void**) &dB, (long int) B_size * sizeof(__half)) )
+    CHECK_CUDA( cudaMalloc((void**) &dC, (long int) C_size * sizeof(__half)) )
+
+    CHECK_CUDA( cudaMemcpy(dA_columns, hA_columns,
+                           (long int) A_num_blocks * sizeof(int),
+                           cudaMemcpyHostToDevice) )
+    CHECK_CUDA( cudaMemcpy(dA_values, hA_values,
+                           (long int) A_ell_cols * A_num_rows * sizeof(__half),
+                           cudaMemcpyHostToDevice) )
+    CHECK_CUDA( cudaMemcpy(dB, hB, (long int) B_size * sizeof(__half),
+                           cudaMemcpyHostToDevice) )
+    CHECK_CUDA( cudaMemcpy(dC, hC, (long int) C_size * sizeof(__half),
+                           cudaMemcpyHostToDevice) )
+    //--------------------------------------------------------------------------
+    // CUSPARSE APIs
+    cusparseHandle_t     handle = NULL;
+    cusparseSpMatDescr_t matA;
+    cusparseDnMatDescr_t matB, matC;
+    void*                dBuffer    = NULL;
+    size_t               bufferSize = 0;
+    CHECK_CUSPARSE( cusparseCreate(&handle) )
+    // Create sparse matrix A in blocked ELL format
+    CHECK_CUSPARSE( cusparseCreateBlockedEll(
+                                      &matA,
+                                      A_num_rows, A_num_cols, A_ell_blocksize,
+                                      A_ell_cols, dA_columns, dA_values,
+                                      CUSPARSE_INDEX_32I,
+                                      CUSPARSE_INDEX_BASE_ZERO, CUDA_R_16F) )
+    // Create dense matrix B
+    CHECK_CUSPARSE( cusparseCreateDnMat(&matB, A_num_cols, B_num_cols, ldb, dB,
+                                        CUDA_R_16F, CUSPARSE_ORDER_COL) )
+    // Create dense matrix C
+    CHECK_CUSPARSE( cusparseCreateDnMat(&matC, A_num_rows, B_num_cols, ldc, dC,
+                                        CUDA_R_16F, CUSPARSE_ORDER_COL) )
+    // allocate an external buffer if needed
+    CHECK_CUSPARSE( cusparseSpMM_bufferSize(
+                                 handle,
+                                 CUSPARSE_OPERATION_NON_TRANSPOSE,
+                                 CUSPARSE_OPERATION_NON_TRANSPOSE,
+                                 &alpha, matA, matB, &beta, matC, CUDA_R_32F,
+                                 CUSPARSE_SPMM_ALG_DEFAULT, &bufferSize) )
+    CHECK_CUDA( cudaMalloc(&dBuffer, bufferSize) )
+
+    // execute SpMM
+    CHECK_CUSPARSE( cusparseSpMM(handle,
+                                 CUSPARSE_OPERATION_NON_TRANSPOSE,
+                                 CUSPARSE_OPERATION_NON_TRANSPOSE,
+                                 &alpha, matA, matB, &beta, matC, CUDA_R_32F,
+                                 CUSPARSE_SPMM_ALG_DEFAULT, dBuffer) )
+
+    // destroy matrix/vector descriptors
+    CHECK_CUSPARSE( cusparseDestroySpMat(matA) )
+    CHECK_CUSPARSE( cusparseDestroyDnMat(matB) )
+    CHECK_CUSPARSE( cusparseDestroyDnMat(matC) )
+    CHECK_CUSPARSE( cusparseDestroy(handle) )
+    //--------------------------------------------------------------------------
+    // device result check
+    CHECK_CUDA( cudaMemcpy(hC, dC, (long int) C_size * sizeof(__half),
+                           cudaMemcpyDeviceToHost) )
+                           
+    /*std::ofstream outputFile("output.txt");
+    for (int i = 0; i < C_size; ++i) {
+        outputFile << static_cast<float>(hC[i]) << std::endl;
+    }*/
+
+    
+    std::printf("spmm_blockedell_example PASSED\n");
+    
+    //--------------------------------------------------------------------------
+    // device memory deallocation
+    CHECK_CUDA( cudaFree(dBuffer) )
+    CHECK_CUDA( cudaFree(dA_columns) )
+    CHECK_CUDA( cudaFree(dA_values) )
+    CHECK_CUDA( cudaFree(dB) )
+    CHECK_CUDA( cudaFree(dC) )
+    
+    delete[] hA_columns;
+    delete[] hA_values;
+    delete[] hB;
+    delete[] hC;
+    return EXIT_SUCCESS;
+}
